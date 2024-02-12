bbb97958cee266c72726d681f3fdd0e2d0598b94
Eduardo Gomes Sanglard <eduardo.sanglard@hotmail.com>
2024-02-12 17:16:02 +0000

    Add prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone Tabnews - Notícias na Aba!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Nóticas na Aba - Em Construção!</h1>
+  return <h1>Nóticas na Aba - Em Construção!</h1>;
 }
 
-export default Home; 
+export default Home;
