31621b37bfd4a883ea6e40752018e8c1e61d3d19
Eduardo Gomes Sanglard <eduardo.sanglard@hotmail.com>
2024-02-18 21:27:19 +0000

    Added database.js

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,4 +1,5 @@
 test("GET to /api/v1/status returns 200", async () => {
-  const response = await fetch("/api/v1/status");
+  console.log("Testando status 200");
+  const response = await fetch("https://localhost/api/v1/status");
   expect(response.status).toBe(200);
 });
