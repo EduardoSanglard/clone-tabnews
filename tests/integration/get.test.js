7680f6df1af77ec2b3b47bc876e67c51b4e40483
Eduardo Gomes Sanglard <eduardo.sanglard@hotmail.com>
2024-02-18 21:33:52 +0000

    Fix .env

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,5 +1,5 @@
 test("GET to /api/v1/status returns 200", async () => {
   console.log("Testando status 200");
-  const response = await fetch("https://localhost/api/v1/status");
+  const response = await fetch("https://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
